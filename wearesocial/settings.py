a72f921b37d3a685e3b30ef3cdbde1095a137720
AoifeMc <aoifemcevoy@gmail.com>
2015-12-15 13:30:24 +0000

    updated settings with logging details

--- wearesocial/settings.py
+++ wearesocial/settings.py
@@ -144,21 +144,6 @@ TEMPLATE_DEBUG = False
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '%s/debug.log' % BASE_DIR,
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
 }
 
 try:
