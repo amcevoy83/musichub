c20499e068fc258d7e5d14b9e72597ad7627fd32
AoifeMc <aoifemcevoy@gmail.com>
2015-12-15 16:35:53 +0000

    update static files reference file

--- wearesocial/settings.py
+++ wearesocial/settings.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = (
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
+STATIC_ROOT = os.path.join(BASE_DIR, "static"),
 
 MEDIA_URL = '/media/'
 
