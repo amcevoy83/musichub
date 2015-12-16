5bdd55c52fbedf6efcb2bd6f491f33a5ead1ec57
AoifeMc <aoifemcevoy@gmail.com>
2015-12-16 12:36:20 +0000

    new admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from contact.models import ContactForm
-from core.models import Register
 
 # Register your models here.
 admin.site.register(ContactForm)
-admin.site.register(Register)
