fe6bc0962dcdcdb5f0a04f876956262e8f492603
AoifeMc <aoifemcevoy@gmail.com>
2015-12-16 14:39:59 +0000

    settings & base

--- wearesocial/settings.py
+++ wearesocial/settings.py
@@ -119,7 +119,7 @@ STATICFILES_DIRS = (
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 
