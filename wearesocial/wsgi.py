5da965e9ed0a38a8c1e391abf80ca0a941b0f061
AoifeMc <aoifemcevoy@gmail.com>
2015-12-16 12:49:36 +0000

    wsgi

--- wearesocial/wsgi.py
+++ wearesocial/wsgi.py
@@ -12,8 +12,8 @@ import django
 django.setup()
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wearesocial.settings")
-os.environ.setdefault["DJANGO_SETTINGS_MODULE"] = "wearesocial.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wearesocial.settings")
+#os.environ.setdefault["DJANGO_SETTINGS_MODULE"] = "wearesocial.settings"
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wearesocial.settings")
 
